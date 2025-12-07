30047b389d77d4221972763bb457e4fc82acd6b5
Дмитрий <dmmurzaev@mail.ru>
2025-12-07 22:54:51 +0500

    Update index.js

--- index.js
+++ index.js
@@ -84,5 +84,5 @@ formElement.addEventListener("submit", (event) => {
   listElement.prepend(newItem);
   const items = getTasksFromDOM();
   saveTasks(items);
-  inputElement.value = "";
+  formElement.reset();
 });
